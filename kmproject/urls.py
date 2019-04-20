74a9d3eed6c7aa2d509a0633aa2f5bbf74063ee9
Fabian Ardila <fabianra97@gmail.com>
2019-04-20 13:52:56 -0500

    Configure server

--- kmproject/urls.py
+++ kmproject/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    path('kmsurvey', include('kmsurvey.urls')),
     path('admin/', admin.site.urls),
 ]
