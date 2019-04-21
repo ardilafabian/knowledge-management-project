9029dfd42e6069fda39788d8115bb104d6aedb0f
Fabian Ardila <fabianra97@gmail.com>
2019-04-21 15:04:24 -0500

    Update and populate models

--- kmsurvey/models.py
+++ kmsurvey/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 
 class Approach(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     score = models.FloatField()
 
     def __str__(self):
-        return '[Approach: %s, Score: %f]' % (self.name, self.score)
+        return '[ID: %d, Approach: %s, Score: %f]' % (self.id, self.name, self.score)
 
 class Tool(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     score = models.FloatField()
     approach = models.ForeignKey(Approach, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '[Tool: %s, Score: %f, Approach: %s]' % (self.name, self.score, self.approach)
+        return '[ID: %d, Tool: %s, Score: %f, Approach: %s]' % (self.id, self.name, self.score, self.approach)
 
 class Scale_Choice(models.Model):
+    id = models.AutoField(primary_key=True)
+    #----------
     TOTALLY_AGREE = 5
     AGREE = 4
     NEITHER_AGREE_NOR_DISAGREE = 3
@@ -30,15 +34,19 @@ class Scale_Choice(models.Model):
         (TOTALLY_DISAGREE, 'Totally disagree'),
     )
 
-    scale_choice = models.PositiveIntegerField(
+    scale_choice_txt = models.CharField(max_length=30, default='null')
+
+    scale_choice_value = models.PositiveIntegerField(
         choices = SCALE_CHOICES,
         default = TOTALLY_AGREE,
     )
 
     def __str__(self):
-        return '[SCALE - Value: %d]' % (scale_choice)
+        return '[Choice: %s, Value: %d]' % (self.scale_choice_txt, self.scale_choice_value)
 
 class Dicotomic_Choice(models.Model):
+    id = models.AutoField(primary_key=True)
+    #----------
     YES = 5
     NO = 1
 
@@ -47,18 +55,28 @@ class Dicotomic_Choice(models.Model):
         (NO, 'No'),
     )
 
-    dicotomic_choice = models.PositiveIntegerField(
+    dicotomic_choice_txt = models.CharField(max_length=5, default='null')
+
+    dicotomic_choice_value = models.PositiveIntegerField(
         choices = DICOTOMIC_CHOICES,
         default = YES,
     )
 
     def __str__(self):
-        return '[DICOTOMIC - Value: %d]' % (dicotomic_choice)
+        return '[Choice: %s, Value: %d]' % (self.dicotomic_choice_txt, self.dicotomic_choice_value)
 
 class Scale_Question(models.Model):
+    id = models.AutoField(primary_key=True)
     question_text = models.CharField(max_length=200)
     choice = models.ForeignKey(Scale_Choice, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '[ID: %d, Question: %s, Choice: %s]' % (self.id, self.question_text, self.choice)
+
 class Dicotomic_Question(models.Model):
+    id = models.AutoField(primary_key=True)
     question_text = models.CharField(max_length=200)
     choice = models.ForeignKey(Dicotomic_Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '[ID: %d, Question: %s, Choice: %s]' % (self.id, self.question_text, self.choice)
