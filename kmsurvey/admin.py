16351194895706d97a31e2ba6806dddac4d7d97a
Fabian Ardila <fabianra97@gmail.com>
2019-04-21 15:34:17 -0500

    Configure admin

--- kmsurvey/admin.py
+++ kmsurvey/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Approach,Tool,Scale_Choice,Dicotomic_Choice,Scale_Question,Dicotomic_Question
 
-# Register your models here.
+admin.site.register(Approach)
+admin.site.register(Tool)
+admin.site.register(Scale_Choice)
+admin.site.register(Dicotomic_Choice)
+admin.site.register(Scale_Question)
+admin.site.register(Dicotomic_Question)
